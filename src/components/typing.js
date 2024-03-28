4e70e5b2b8ad67c7962d9074c92d9b219a44bf09
Keren Villatoro <kerenv@outlook.com>
2024-03-28 12:25:13 -0700

    initialization

---
+++ src/components/typing.js
@@ -0,0 +1,15 @@
+'use client'
+import React, { useState, useEffect, useRef } from "react";
+
+function Typing() {
+
+
+    return (
+        <>
+        <div> Typing Component </div>
+        </>
+    )
+    
+}
+
+export default Typing;
